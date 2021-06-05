01bb6e08f4136747555a12b26d6d287670f16ac6
Laica-Lunasys <laica.lunasys@gmail.com>
2021-06-05 23:52:04 +0900

    Wait establish interface

--- utils/static-ip.ps1
+++ utils/static-ip.ps1
@@ -12,18 +12,32 @@ if (-Not($WindowsPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole] "Adm
 }
 $erroractionpreference = "stop"
 
+$TargetInterface = "vEthernet (WSL)"
+
 # ---
 # Bind to Host ($HostAddress -> host)
 # netsh interface ip add address "vEthernet (WSL)" $HostAddress 255.255.255.0
 # Check: netsh interface ip show addresses
 # without netsh help: Get-Command -Module NetTCPIP
+$Timeout = 60
+$ElapsedTime = 0
+Write-Output ":: Waiting Interface Up..."
+while (!(Get-NetIPAddress -InterfaceAlias $TargetInterface -ErrorAction 'SilentlyContinue')) {
+    if ($ElapsedTime -gt $Timeout) {
+        Write-Output "!! E: Interface configure timed out"
+        Write-Output "!! E: Please check configure correctly: $TargetInterface"
+        Write-Error "Failed detect interface"
+    }
+    Start-Sleep 1
+    $ElapsedTime += 1
+}
 
-if (Get-NetIPAddress -InterfaceAlias "vEthernet (WSL)" -AddressFamily IPv4 | Where-Object { $_.IPAddress -eq $HostAddress }) {
+if (Get-NetIPAddress -InterfaceAlias $TargetInterface -AddressFamily IPv4 | Where-Object { $_.IPAddress -eq $HostAddress }) {
     Write-Output "-- Already binded?"
 }
 else {
     Write-Output ":: Add IP..."
-    New-NetIPAddress -InterfaceAlias "vEthernet (WSL)" -AddressFamily IPv4 -IPAddress $HostAddress -PrefixLength 24
+    New-NetIPAddress -InterfaceAlias $TargetInterface -AddressFamily IPv4 -IPAddress $HostAddress -PrefixLength 24
 }
 
 # Set Static IP for WSL2 Environment
